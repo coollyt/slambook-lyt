dbb2d8aaf6b64926076c7b04b0adf5794632776c
cool <2076993278@qq.com>
2024-03-21 21:24:36 +0800

    first commit

---
+++ ch7/feature_extraction.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main ( int argc, char** argv)
+{
+   if ( argc != 3 )
+   { 
+       cout<<"usage: feature_extraction img1 img2"<<endl;
+       return 1;
+    }
+    //-- 读取图像
+    Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
+    Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
+    //-- 读取图像
+    Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
+    Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
+
+    //-- 初始化
+    std::vector<KeyPoint> keypoints_1, keypoints_2;
+    Mat descriptors_1, descriptors_2;
+    
+    printf ( "-- Max dist : %f \n", max_dist );
+    printf ( "-- Min dist : %f \n", min_dist );
+
+    //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
+    std::vector< DMatch > good_matches;
+    for ( int i = 0; i < descriptors_1.rows; i++ )
+    {
+        if ( matches[i].distance <= max ( 2*min_dist, 30.0 ) )
+        {
+            good_matches.push_back ( matches[i] );
+        }
+    }
+
+    //-- 第五步:绘制匹配结果
+    Mat img_match;
+    Mat img_goodmatch;
+    drawMatches ( img_1, keypoints_1, img_2, keypoints_2, matches, img_match );
+    drawMatches ( img_1, keypoints_1, img_2, keypoints_2, good_matches, img_goodmatch );
+    imshow ( "所有匹配点对", img_match );
+    imshow ( "优化后匹配点对", img_goodmatch );
+    waitKey(0);
+
+    return 0;
+}
